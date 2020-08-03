0e8215f2d67569c6113dd4fba1dc5f500833e696
Griffin O'Neill <gsoneill1003@gmail.com>
2020-08-03 14:25:01 -0400

    More functionality added

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 #[macro_use]
 extern crate lazy_static;
 
-use std::{fs, fmt};
+use std::{fs, fmt::Display};
 use midly::{Smf, EventKind::*, MidiMessage::*, number::u7};
 use image::{DynamicImage, GenericImageView, GenericImage};
 
@@ -71,96 +71,204 @@ load_note!(Eb5, 75);
 fn main() -> Result<(), image::error::ImageError>
 {
     const spacing: usize = 10;
-    let fingering_chart = load_midi(MIDI_PATH);
-    fingering_chart.generate_chart(OUTPUT_PATH, NOTES_PER_ROW, spacing)?;
+    let fingering_chart = Song::load(MIDI_PATH);
+    fingering_chart.output_entire(OUTPUT_PATH, NOTES_PER_ROW, spacing)?;
     println!("{}", fingering_chart);
     Ok(())
 }
 
-/// Parse a midi file. Generate a list containing all tracks. Tracks themselves are simply lists of notes.
-fn load_midi(midi_path: &str) -> Song
-{
-    // Parse a midi file
-    let raw_data = fs::read(midi_path).unwrap();
-    let midi = Smf::parse(&raw_data).unwrap();
-
-    // Use the information
-    println!("midi file has {} tracks", midi.tracks.len());
-
-    // Iterate through the midi file and collect notes
-    Song {tracks: midi.tracks
-        .iter()
-        .map(|track| track
-            .iter()
-            .filter_map(|event| if let Midi { channel: _, message: NoteOn {key, vel: _} } = event.kind { Note::get(key) } else { None })
-            .collect::<Vec<&Note>>())
-        .filter_map(|notes| if notes.is_empty() { None } else { Some(Track{notes}) })
-        .collect::<Vec<Track>>()}
-}
-
-struct Song
+/// Entire song, jsut a vec of tracks with some methods
+pub struct Song
 {
     tracks: Vec<Track>
 }
 
+/// Struct used for tracks
+pub struct Track
+{
+    notes: Vec<&'static Note>
+}
+
+/// Struct used for individual notes
+pub struct Note
+{
+    name: String,
+    byte: u8,
+    image: image::DynamicImage
+}
+
 impl Song
 {
+    /// Parse a midi file. Generate a list containing all tracks. Tracks themselves are simply lists of notes.
+    pub fn load(midi_path: &str) -> Song
+    {
+        // Parse a midi file
+        let raw_data = fs::read(midi_path).expect("Failed to load midi data");
+        let midi = Smf::parse(&raw_data).expect("Failed to parse midi data");
+
+        // Iterate through the midi file and collect notes
+        Song {tracks: midi.tracks
+            .iter()
+            .map(|track| track
+                .iter()
+                .filter_map(|event| if let Midi { channel: _, message: NoteOn {key, vel: _} } = event.kind { Note::get(key) } else { None })
+                .collect::<Vec<&Note>>())
+            .filter_map(|notes| if notes.is_empty() { None } else { Some(Track{notes}) })
+            .collect::<Vec<Track>>()}
+    }
+
+    /// Output chart with each cell as an individual file
+    pub fn output_cells(&self, output_path: &str) -> Result<(), image::error::ImageError>
+    {
+        for track_index in 0..self.tracks.len()
+        {
+            let track_path = format!("{}/track{}", output_path, track_index);
+            fs::create_dir_all(&track_path)?;
+            for (cell, note) in self.tracks[track_index].notes.iter().enumerate() 
+            {
+                note.image.save(format!("{}/cell{}.png", track_path, cell))?;
+            }
+        }
+        Ok(())
+    }
+
+    /// Output chart with each row as an individual file
+    pub fn output_rows(&self, output_path: &str, notes_per_row: usize, spacing: usize) -> Result<(), image::error::ImageError>
+    {
+        for track_index in 0..self.tracks.len()
+        {
+            let track_path = format!("{}/track{}", output_path, track_index);
+            fs::create_dir_all(&track_path)?;
+            for (row, image) in self.tracks[track_index].row_images(notes_per_row, spacing).iter().enumerate() 
+            {
+                image.save(format!("{}/row{}.png", track_path, row))?;
+            }
+        }
+        Ok(())
+    }
+
     /// Generate a chart and output to the given directory.
-    fn generate_chart(&self, output_path: &str, notes_per_row: usize, spacing: usize) -> Result<(), image::error::ImageError>
+    pub fn output_entire(&self, output_path: &str, notes_per_row: usize, spacing: usize) -> Result<(), image::error::ImageError>
     {
         fs::create_dir_all(output_path)?;
         for track_index in 0..self.tracks.len()
         {
-            let track = &self.tracks[track_index];
-            let row_images: Vec<DynamicImage> = (0..track.notes.len())
-                .take_while(|row| row * notes_per_row < track.notes.len())
-                .map(|row| 
-                {
-                    let row_notes: Vec<&Note> = (0..notes_per_row)
-                        .take_while(|col| col + (row * notes_per_row) < track.notes.len())
-                        .map(|col| track.notes[(row * notes_per_row) + col])
-                        .collect();
-
-                    // Create an image
-                    let width: u32 = row_notes.iter().map(|note| note.image.width()).sum::<u32>() + row_notes.len() as u32 * spacing as u32;
-                    let height: u32 = row_notes.iter().map(|note| note.image.height()).max().unwrap_or(0);
-                    let mut row_image: DynamicImage = DynamicImage::new_rgb8(width, height);
-
-                    // Draw the image
-                    let mut current_x: u32 = 0;
-                    for note in row_notes
-                    {
-                        row_image.copy_from(&note.image, current_x, 0).unwrap();
-                        current_x += note.image.width() + spacing as u32;
-                    }
-
-                    row_image
-                })
-                .collect();
-
-            // Create an image with all notes of the entire track
-            let width: u32 = row_images.iter().map(|image| image.width()).max().unwrap_or(0);
-            let height: u32 = row_images.iter().map(|image| image.height()).sum::<u32>() + (row_images.len() * spacing) as u32;
-            let mut track_image: DynamicImage = DynamicImage::new_rgb8(width, height);
-
-            // Draw the image
-            let mut current_y: u32 = 0;
-            for row_image in row_images
-            {
-                track_image.copy_from(&row_image, 0, current_y)?;
-                current_y += row_image.height() + spacing as u32;
-            }
-
-            // Write the image
+            let track_image = self.tracks[track_index].track_image(notes_per_row, spacing);
             track_image.save(format!("{}/track{}.png", output_path, track_index))?;
         }
         Ok(())
     }
 }
 
-impl fmt::Display for Song
+impl Track
 {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result 
+    /// Generates an image for the entire track
+    pub fn track_image(&self, notes_per_row: usize, spacing: usize) -> DynamicImage
+    {
+        let mut track_image: DynamicImage = DynamicImage::new_rgb8(0, spacing as u32);
+        for row_image in self.row_images(notes_per_row, spacing)
+        {
+            let previous = track_image;
+            track_image = DynamicImage::new_rgb8(std::cmp::max(previous.width(), row_image.width()), previous.height() + row_image.height() + spacing as u32);
+            track_image.copy_from(&previous, 0, 0).expect("Failed to generate track image");
+            track_image.copy_from(&row_image, 0, previous.height()).expect("Failed to generate track image");
+        }
+        track_image
+    }
+
+    /// Generates images in rows
+    pub fn row_images(&self, notes_per_row: usize, spacing: usize) -> Vec<DynamicImage>
+    {
+        (0..self.notes.len())
+            .take_while(move |row| row * notes_per_row < self.notes.len())
+            .map(move |row| 
+            {
+                let row_notes: Vec<&Note> = (0..notes_per_row)
+                    .take_while(|col| col + (row * notes_per_row) < self.notes.len())
+                    .map(|col| self.notes[(row * notes_per_row) + col])
+                    .collect();
+
+                // Create an image
+                let width: u32 = row_notes.iter().map(|note| note.image.width()).sum::<u32>() + row_notes.len() as u32 * spacing as u32 + spacing as u32;
+                let height: u32 = row_notes.iter().map(|note| note.image.height()).max().unwrap_or(0);
+                let mut row_image: DynamicImage = DynamicImage::new_rgb8(width, height);
+
+                // Draw the image
+                let mut current_x: u32 = spacing as u32;
+                for note in row_notes
+                {
+                    row_image.copy_from(&note.image, current_x, 0).expect("Failed to generate row image");
+                    current_x += note.image.width() + spacing as u32;
+                }
+
+                row_image
+            }).collect()
+    }
+
+    /// Returns an iterator into cell images. The images themselves are 
+    /// generated at load-time, so this method is low-cost.
+    pub fn cell_images(&self) -> impl Iterator<Item = &DynamicImage>
+    {
+        self.notes.iter().map(|note| &note.image)
+    }
+}
+
+impl Note
+{
+    /// Note constructor. Should only be called in a lazy_static way
+    fn new(byte: u8, image_path: String) -> Note
+    {
+        let image = image::open(&image_path).expect(&format!("Failed to read {}", image_path));
+        let name = image_path[image_path.rfind('/').expect("Bad path") + 1 .. image_path.len() - 4].to_string();
+        Note { name, byte, image }
+    }
+
+    /// Access a note via it's midi byte index. To support additional notes, they must be added to this function.
+    fn get(index: u7) -> Option<&'static Note>
+    {
+        match index.as_int()
+        {
+            i if i == Ab2.byte => Some(&Ab2),
+            i if i == A2.byte  => Some(&A2),
+            i if i == Bb2.byte => Some(&Bb2),
+            i if i == B2.byte  => Some(&B2),
+            i if i == C3.byte  => Some(&C3),
+            i if i == Db3.byte => Some(&Db3),
+            i if i == D3.byte  => Some(&D3), 
+            i if i == Eb3.byte => Some(&Eb3), 
+            i if i == E3.byte  => Some(&E3),
+            i if i == F3.byte  => Some(&F3),
+            i if i == Gb3.byte => Some(&Gb3),
+            i if i == G3.byte  => Some(&G3),
+            i if i == Ab3.byte => Some(&Ab3),
+            i if i == A3.byte  => Some(&A3),
+            i if i == Bb3.byte => Some(&Bb3),
+            i if i == B3.byte  => Some(&B3),
+            i if i == C4.byte  => Some(&C4),
+            i if i == Db4.byte => Some(&Db4),
+            i if i == D4.byte  => Some(&D4),
+            i if i == Eb4.byte => Some(&Eb4),
+            i if i == E4.byte  => Some(&E4),
+            i if i == F4.byte  => Some(&F4),
+            i if i == Gb4.byte => Some(&Gb4),
+            i if i == G4.byte  => Some(&G4),
+            i if i == Ab4.byte => Some(&Ab4),
+            i if i == A4.byte  => Some(&A4),
+            i if i == Bb4.byte => Some(&Bb4),
+            i if i == B4.byte  => Some(&B4),
+            i if i == C5.byte  => Some(&C5),
+            i if i == Db5.byte => Some(&Db5),
+            i if i == D5.byte  => Some(&D5),
+            i if i == Eb5.byte => Some(&Eb5),
+            _ => None
+        }
+    }
+}
+
+/// Prints every track in a song
+impl Display for Song
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result 
     {
         for track in self.tracks.iter()
         {
@@ -173,15 +281,10 @@ impl fmt::Display for Song
     }
 }
 
-/// Struct used for tracks
-struct Track
+/// Prints every note in a track
+impl Display for Track
 {
-    notes: Vec<&'static Note>
-}
-
-impl fmt::Display for Track
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result 
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result 
     {
         for note in self.notes.iter()
         {
@@ -194,68 +297,10 @@ impl fmt::Display for Track
     }
 }
 
-/// Struct used for individual notes
-struct Note
+/// Prints the name of a note
+impl Display for Note
 {
-    name: String,
-    byte: u8,
-    image: image::DynamicImage
-}
-
-impl Note
-{
-    fn new(byte: u8, image_path: String) -> Note
-    {
-        let image = image::open(&image_path).expect(&format!("Failed to read {}", image_path));
-        let name = image_path[image_path.rfind('/').unwrap() + 1 .. image_path.len() - 4].to_string();
-        Note { name, byte, image }
-    }
-
-    /// Access a note via it's midi byte index.
-    fn get(index: u7) -> Option<&'static Note>
-    {
-        match index.as_int()
-        {
-            i if i == Ab2.byte => Some(&Ab2),
-            i if i == A2.byte => Some(&A2),
-            i if i == Bb2.byte => Some(&Bb2),
-            i if i == B2.byte => Some(&B2),
-            i if i == C3.byte => Some(&C3),
-            i if i == Db3.byte => Some(&Db3),
-            i if i == D3.byte => Some(&D3), 
-            i if i == Eb3.byte => Some(&Eb3), 
-            i if i == E3.byte => Some(&E3),
-            i if i == F3.byte => Some(&F3),
-            i if i == Gb3.byte => Some(&Gb3),
-            i if i == G3.byte => Some(&G3),
-            i if i == Ab3.byte => Some(&Ab3),
-            i if i == A3.byte => Some(&A3),
-            i if i == Bb3.byte => Some(&Bb3),
-            i if i == B3.byte => Some(&B3),
-            i if i == C4.byte => Some(&C4),
-            i if i == Db4.byte => Some(&Db4),
-            i if i == D4.byte => Some(&D4),
-            i if i == Eb4.byte => Some(&Eb4),
-            i if i == E4.byte => Some(&E4),
-            i if i == F4.byte => Some(&F4),
-            i if i == Gb4.byte => Some(&Gb4),
-            i if i == G4.byte => Some(&G4),
-            i if i == Ab4.byte => Some(&Ab4),
-            i if i == A4.byte => Some(&A4),
-            i if i == Bb4.byte => Some(&Bb4),
-            i if i == B4.byte => Some(&B4),
-            i if i == C5.byte => Some(&C5),
-            i if i == Db5.byte => Some(&Db5),
-            i if i == D5.byte => Some(&D5),
-            i if i == Eb5.byte => Some(&Eb5),
-            _ => None
-        }
-    }
-}
-
-impl fmt::Display for Note
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result 
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result 
     {
         write!(f, "{}", self.name)
     }
