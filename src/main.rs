187beb0dd67849a18c1bfc9c58d8b399a2e1d0c0
Griffin O'Neill <gsoneill1003@gmail.com>
2020-08-05 23:16:00 -0400

    Improved the macro for loading notes

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 #![allow(non_upper_case_globals)]
+
 #[macro_use]
 extern crate lazy_static;
 
-use std::{fs, fmt::Display};
+use std::{fs, fmt::Display, collections::HashSet};
 use midly::{Smf, EventKind::*, MidiMessage::*, number::u7};
 use image::{DynamicImage, GenericImageView, GenericImage};
 
 const SPACING: usize = 10;
+// TODO: Find a way to let chart dir (and others) be set in a configuration file
 const CHART_DIR: &str = "C:/Project/Workspace/Rust/fingering_chart/res/fingerings";
 const OUTPUT_PATH: &str = "C:/Project/Workspace/Rust/fingering_chart/out";
-const MIDI_PATH: &str = "C:/Project/Workspace/Rust/fingering_chart/res/kass_notes.mid";
-const NOTES_PER_ROW: usize = 7;
+//const MIDI_PATH: &str = "C:/Project/Workspace/Rust/fingering_chart/res/kass_notes.mid";
+const MIDI_PATH: &str = "C:/Project/Workspace/Rust/fingering_chart/res/bramble.mid";
+const NOTES_PER_ROW: usize = 14;
 
 // TODO: Add altissimo fingerings
 // TODO: Add trill fingerings
@@ -18,59 +21,51 @@ const NOTES_PER_ROW: usize = 7;
 // Then try semitones (with warning). If impossible, leave out notes (with
 // warning).
 // TODO: Make this more general, right not it's hard-coded for the tenor sax (Bb transposed)
-
-/// Macro to load notes using lazy_static
-#[macro_export]
-macro_rules! load_note
-{
-    ($name: ident, $num: literal) => 
-    {
-        lazy_static!
-        {
-            static ref $name: Note = Note::new($num, format!("{}/{}.png", CHART_DIR, stringify!($name)));
-        }
-    }
-}
+// FIXME: This silently removes notes outside of range!
 
 // Note constants
-load_note!(Ab2, 44);
-load_note!(A2, 45);
-load_note!(Bb2, 46);
-load_note!(B2, 47);
-load_note!(C3, 48); 
-load_note!(Db3, 49); 
-load_note!(D3, 50); 
-load_note!(Eb3, 51); 
-load_note!(E3, 52); 
-load_note!(F3, 53); 
-load_note!(Gb3, 54); 
-load_note!(G3, 55); 
-load_note!(Ab3, 56); 
-load_note!(A3, 57); 
-load_note!(Bb3, 58); 
-load_note!(B3, 59);
-load_note!(C4, 60); 
-load_note!(Db4, 61); 
-load_note!(D4, 62); 
-load_note!(Eb4, 63); 
-load_note!(E4, 64); 
-load_note!(F4, 65); 
-load_note!(Gb4, 66); 
-load_note!(G4, 67); 
-load_note!(Ab4, 68); 
-load_note!(A4, 69); 
-load_note!(Bb4, 70); 
-load_note!(B4, 71);
-load_note!(C5, 72); 
-load_note!(Db5, 73); 
-load_note!(D5, 74); 
-load_note!(Eb5, 75);
-// Altissimo from here beyond: 
-// load_note!(E5, 76);
+load_notes!
+{
+    Ab2, 44,
+    A2, 45, 
+    Bb2, 46, 
+    B2, 47, 
+    C3, 48,  
+    Db3, 49,  
+    D3, 50,  
+    Eb3, 51,  
+    E3, 52,  
+    F3, 53,  
+    Gb3, 54,  
+    G3, 55,  
+    Ab3, 56,  
+    A3, 57,  
+    Bb3, 58,  
+    B3, 59, 
+    C4, 60,  
+    Db4, 61,  
+    D4, 62,  
+    Eb4, 63,  
+    E4, 64,  
+    F4, 65,  
+    Gb4, 66,  
+    G4, 67,  
+    Ab4, 68,  
+    A4, 69,  
+    Bb4, 70,  
+    B4, 71, 
+    C5, 72,  
+    Db5, 73,  
+    D5, 74,  
+    Eb5, 75
+    // Altissimo from here beyond: 
+    // E5, 76
+}
 
 /// Entry-point
 fn main() -> Result<(), image::error::ImageError>
 {
+    //let args: Vec<String> = env::args().collect();
     let fingering_chart = Song::load(MIDI_PATH);
     fingering_chart.output_entire(OUTPUT_PATH, NOTES_PER_ROW, SPACING)?;
     println!("{}", fingering_chart);
@@ -106,12 +101,15 @@ impl Song
         let raw_data = fs::read(midi_path).expect("Failed to load midi data");
         let midi = Smf::parse(&raw_data).expect("Failed to parse midi data");
 
+        // Keep track of notes
+        let mut notes: HashSet<u8> = HashSet::new();
+
         // Iterate through the midi file and collect notes
         Song {tracks: midi.tracks
             .iter()
             .map(|track| track
                 .iter()
-                .filter_map(|event| if let Midi { channel: _, message: NoteOn {key, vel: _} } = event.kind { Note::get(key) } else { None })
+                .filter_map(|event| if let Midi { channel: _, message: NoteOn {key, vel: _} } = event.kind { Note::get(key, &mut notes) } else { None })
                 .collect::<Vec<&Note>>())
             .filter_map(|notes| if notes.is_empty() { None } else { Some(Track{notes}) })
             .collect::<Vec<Track>>()}
@@ -126,7 +124,7 @@ impl Song
             fs::create_dir_all(&track_path)?;
             for (cell, note) in self.tracks[track_index].notes.iter().enumerate() 
             {
-                note.image.save(format!("{}/cell{}.png", track_path, cell))?;
+                note.image.save(format!("{}/{}.png", track_path, cell))?;
             }
         }
         Ok(())
@@ -209,54 +207,42 @@ impl Track
     }
 }
 
-impl Note
+/// Macro to load notes using lazy_static
+#[macro_export]
+macro_rules! load_notes
 {
-    /// Note constructor. Should only be called in a lazy_static way
-    fn new(byte: u8, image_path: String) -> Note
+    ($($name: ident, $num: literal),*) => 
     {
-        let image = image::open(&image_path).expect(&format!("Failed to read {}", image_path));
-        let name = image_path[image_path.rfind('/').expect("Bad path") + 1 .. image_path.len() - 4].to_string();
-        Note { name, byte, image }
-    }
-
-    /// Access a note via it's midi byte index. To support additional notes, they must be added to this function.
-    fn get(index: u7) -> Option<&'static Note>
-    {
-        match index.as_int()
+        lazy_static!
         {
-            i if i == Ab2.byte => Some(&Ab2),
-            i if i == A2.byte  => Some(&A2),
-            i if i == Bb2.byte => Some(&Bb2),
-            i if i == B2.byte  => Some(&B2),
-            i if i == C3.byte  => Some(&C3),
-            i if i == Db3.byte => Some(&Db3),
-            i if i == D3.byte  => Some(&D3), 
-            i if i == Eb3.byte => Some(&Eb3), 
-            i if i == E3.byte  => Some(&E3),
-            i if i == F3.byte  => Some(&F3),
-            i if i == Gb3.byte => Some(&Gb3),
-            i if i == G3.byte  => Some(&G3),
-            i if i == Ab3.byte => Some(&Ab3),
-            i if i == A3.byte  => Some(&A3),
-            i if i == Bb3.byte => Some(&Bb3),
-            i if i == B3.byte  => Some(&B3),
-            i if i == C4.byte  => Some(&C4),
-            i if i == Db4.byte => Some(&Db4),
-            i if i == D4.byte  => Some(&D4),
-            i if i == Eb4.byte => Some(&Eb4),
-            i if i == E4.byte  => Some(&E4),
-            i if i == F4.byte  => Some(&F4),
-            i if i == Gb4.byte => Some(&Gb4),
-            i if i == G4.byte  => Some(&G4),
-            i if i == Ab4.byte => Some(&Ab4),
-            i if i == A4.byte  => Some(&A4),
-            i if i == Bb4.byte => Some(&Bb4),
-            i if i == B4.byte  => Some(&B4),
-            i if i == C5.byte  => Some(&C5),
-            i if i == Db5.byte => Some(&Db5),
-            i if i == D5.byte  => Some(&D5),
-            i if i == Eb5.byte => Some(&Eb5),
-            _ => None
+            $( static ref $name: Note = Note::new($num, format!("{}/{}.png", CHART_DIR, stringify!($name))) );*;
+        }
+
+        impl Note
+        {
+            fn new(byte: u8, image_path: String) -> Note
+            {
+                let image = image::open(&image_path).expect(&format!("Failed to read {}", image_path));
+                let name = image_path[image_path.rfind('/').expect("Bad path") + 1 .. image_path.len() - 4].to_string();
+                Note { name, byte, image }
+            }
+
+            /// Access a note via it's midi byte index.
+            fn get(index: u7, notes: &mut HashSet<u8>) -> Option<&'static Note>
+            {
+                match index.as_int()
+                {
+                    $(i if i == $name.byte => Some(&$name)),*,
+                    i => 
+                    {
+                        if notes.insert(i)
+                        {
+                            eprintln!("Note out of range: {}", i);
+                        }
+                        None
+                    }
+                }
+            }
         }
     }
 }
