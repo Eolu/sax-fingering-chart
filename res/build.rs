9123b65de1bf961fa29ac2da4e50d37fa15903cb
Griffin O'Neill <gsoneill1003@gmail.com>
2020-08-14 13:58:41 -0400

    Made an icon

---
+++ res/build.rs
@@ -0,0 +1,16 @@
+#[cfg(windows)]
+extern crate winres;
+
+#[cfg(windows)]
+fn main() 
+{
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("res/icon.ico");
+    res.compile().unwrap();
+}
+
+#[cfg(unix)]
+fn main() 
+{
+    // Intentionally left blank
+}
